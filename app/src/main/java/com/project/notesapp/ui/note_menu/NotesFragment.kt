ee2f40aadc6c58b43107c3f4a7831b324dabb7d2
Rittik <rittikghoshkolkata@gmail.com>
2024-07-27 23:57:50 +0530

    Update login, registration and navigation graph

---
+++ app/src/main/java/com/project/notesapp/ui/note_menu/NotesFragment.kt
@@ -0,0 +1,37 @@
+package com.project.notesapp.ui.note_menu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.notesapp.R
+import com.project.notesapp.databinding.FragmentNotesBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NotesFragment : Fragment() {
+
+    private var _binding: FragmentNotesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNotesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
--- app/src/main/java/com/project/notesapp/ui/notes/Note.kt
+++ app/src/main/java/com/project/notesapp/ui/notes/Note.kt
@@ -2,15 +2,27 @@ package com.project.notesapp.ui.notes
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.navigation.NavigationView
 import com.project.notesapp.R
 import com.project.notesapp.databinding.NoteBinding
+import com.project.notesapp.ui.note_menu.NotesFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,6 +31,8 @@ class Note : Fragment() {
     private var _binding: NoteBinding? = null
     private val binding get() = _binding!!
     private lateinit var context: Context
+
+    private var flag = 1
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -31,14 +45,28 @@ class Note : Fragment() {
     ): View {
         _binding = NoteBinding.inflate(inflater, container, false)
 
-        val actionToggleBar = ActionBarDrawerToggle(
-            requireActivity(),
-            binding.noteDrawer,
-            R.string.open,
-            R.string.close
-        )
-        binding.noteDrawer.addDrawerListener(actionToggleBar)
-        actionToggleBar.syncState()
+        if (savedInstanceState == null) {
+            binding.heading.text = setHeading()
+            childFragmentManager.beginTransaction().replace(R.id.frameLayout, NotesFragment())
+                .commit()
+        }
+
+        binding.navView.setNavigationItemSelectedListener { menuItem ->
+            val fragmentTransaction = childFragmentManager.beginTransaction()
+            when (menuItem.itemId) {
+                R.id.note -> {
+                    binding.heading.text = setHeading()
+                    fragmentTransaction.replace(R.id.frameLayout, NotesFragment())
+                    fragmentTransaction.addToBackStack(null)
+                    fragmentTransaction.commit()
+                    true
+                }
+
+                else -> false
+            }.also {
+                binding.noteDrawer.closeDrawer(GravityCompat.START)
+            }
+        }
 
         return binding.root
     }
@@ -56,11 +84,67 @@ class Note : Fragment() {
         actionToggleBar.syncState()
         (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         (requireActivity() as AppCompatActivity).supportActionBar?.setHomeButtonEnabled(true)
+
+        ((requireActivity()) as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.note -> {
+                        Toast.makeText(context, "Toast", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    R.id.reminder -> {
+                        true
+                    }
+
+                    R.id.label -> {
+                        true
+                    }
+
+                    R.id.bin -> {
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
+    private fun setHeading(): String {
+        var heading = ""
+        when (flag) {
+            1 -> {
+                heading = "Notes"
+            }
 
+            2 -> {
+                heading = "Reminder"
+            }
+
+            3 -> {
+                heading
+            }
+
+            4 -> {
+
+            }
+        }
+        heading = if (flag == 1) {
+            "Notes"
+        } else if (flag == 2) {
+            "Reminder"
+        } else if (flag == 3) {
+            "Label"
+        } else {
+            "Bin"
+        }
+        return heading
     }
 
     override fun onDestroyView() {
